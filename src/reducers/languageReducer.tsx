d0106e921b8e9b6a38cdef6a8e4d466208beddd9
“Omar <djomrofficial@gmail.com>
2022-10-25 14:14:32 +0300

    pushing the latest working version with language context wrapper

--- src/reducers/languageReducer.tsx
+++ src/reducers/languageReducer.tsx
@@ -1,1 +1,28 @@
+import { SET_LANGUAGE } from '../store/actions';
 
+const initialState = {
+	useLanguage: '',
+};
+
+const languageReducer = (
+	state = initialState,
+	action: { type: any; payload: any }
+) => {
+	const { type, payload } = action;
+	switch (type) {
+		case SET_LANGUAGE:
+			return {
+				...state,
+				useLanguage: payload,
+			};
+	}
+};
+
+export const setLanguageAction = (selectedLanguage: string) => {
+	return {
+		type: SET_LANGUAGE,
+		payload: selectedLanguage,
+	};
+};
+
+export default languageReducer;
