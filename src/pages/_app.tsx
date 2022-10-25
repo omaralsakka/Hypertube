53086fa2b7a5bb2434802338b2ce893950ffd9b4
“Omar <djomrofficial@gmail.com>
2022-10-25 17:13:34 +0300

    created signup page with styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 // src/pages/_app.tsx
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import 'i18next';
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -10,12 +13,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from '../store/store';
 import LanguageProvider from '../LanguageProvider';
+import Head from 'next/head';
+
 const MyApp: AppType<{ session: Session | null }> = ({
 	Component,
 	pageProps: { session, ...pageProps },
 }) => {
 	return (
 		<SessionProvider session={session}>
+			<Head>
+				<title>Hypertube</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 			<Provider store={store}>
 				<LanguageProvider>
 					<Component {...pageProps} />
