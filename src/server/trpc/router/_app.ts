b2c9fadae33ba6f19ca3fc5447a1bd0e8873df33
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-10-25 18:04:36 +0300

    Non-working registration enpoint for testing

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,10 +2,12 @@
 import { router } from "../trpc";
 import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
+import { signUpRouter } from "./signup";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  signup: signUpRouter
 });
 
 // export type definition of API
