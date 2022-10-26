4694237550cdb64c4c58da5193904a13830191a2
“Omar <djomrofficial@gmail.com>
2022-10-26 19:21:11 +0300

    home page with cards working, search not working yet

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import NavigationBar from '../../components/navbar';
+import SearchNavBar from '../../components/searchNavBar';
+import { Movies } from '../../types/appTypes';
+import MovieCard from '../../components/moviecard';
 
 const Home = () => {
-	const [movies, setMovies] = useState();
+	const [movies, setMovies] = useState<Movies>();
 	const getMovies = async () => {
 		const response = await fetch('https://yts.mx/api/v2/list_movies.json');
 		const {
@@ -12,7 +17,19 @@ const Home = () => {
 	useEffect(() => {
 		getMovies().then((resp) => setMovies(resp));
 	}, []);
-	return <></>;
+	console.log(movies);
+	return (
+		<>
+			<NavigationBar />
+			<Container className="mb-4">
+				<SearchNavBar />
+			</Container>
+			<Container className="d-flex flex-wrap justify-content-center" fluid>
+				{movies &&
+					movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+			</Container>
+		</>
+	);
 };
 
 export default Home;
