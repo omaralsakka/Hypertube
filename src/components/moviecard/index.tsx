139ef91a4856aef2d867cc7c2701ba1d3bd140e8
“Omar <djomrofficial@gmail.com>
2022-10-27 11:32:56 +0300

    handling not found images case error with default image to displa

--- src/components/moviecard/index.tsx
+++ src/components/moviecard/index.tsx
@@ -2,7 +2,7 @@ import { Card, Container } from 'react-bootstrap';
 import { Movie } from '../../types/appTypes';
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
-	if (!movie.medium_cover_image) {
+	if (!movie) {
 		return <></>;
 	}
 	console.log(movie);
@@ -12,7 +12,15 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
 				className="m-3 movieCard bg-transparent"
 				style={{ maxWidth: '100vw', minWidth: '20vw' }}
 			>
-				<Card.Img src={movie.medium_cover_image} alt="Card image" />
+				<Card.Img
+					src={movie.medium_cover_image}
+					alt="Card image"
+					onError={({ currentTarget }) => {
+						currentTarget.onerror = null;
+						currentTarget.src = '/notfound.png';
+					}}
+				/>
+
 				<Card.ImgOverlay className="p-1 d-flex justify-content-center movieCard-OverLay">
 					<Container fluid className="movieCard-Body mt-auto p-3">
 						<Card.Title className="mb-3">{movie.title}</Card.Title>
