568e9cfe63497db24475d41440ee5def105e19e6
“Omar <djomrofficial@gmail.com>
2022-10-31 15:05:08 +0200

    separated movie info into a separate component

--- src/pages/home/[movieId].tsx
+++ src/pages/home/[movieId].tsx
@@ -1,15 +1,16 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { Container, Card, Row, Col, Collapse, Button } from 'react-bootstrap';
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { RootReducer } from '../../types/appTypes';
 import { useState } from 'react';
 import { Movies, Movie, MovieData } from '../../types/appTypes';
 import MovieCard from '../../components/moviecard';
-import { movieRate, getOmdb } from '../../utils/helperFunctions';
+import { getOmdb } from '../../utils/helperFunctions';
 import { FaPlay } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import CommentsSection from '../../components/commentsSection';
+import MovieInfo from '../../components/movieInfo';
 
 const MoviePage = () => {
 	const router = useRouter();
@@ -21,7 +22,6 @@ const MoviePage = () => {
 	const [isLoading, setLoading] = useState(false);
 	const [movieData, setMovieData] = useState<MovieData>();
 	const [suggestedMovies, setSuggestedMovies] = useState<Movies>();
-	const [openDescription, setOpenDescription] = useState(false);
 	// this is forced comments to display for now till we got real backend comments
 	const [comments, setComments] = useState([
 		{
@@ -92,6 +92,7 @@ const MoviePage = () => {
 		},
 	]);
 	let i = 0;
+
 	const suggestedMovieStyle = {
 		maxWidth: '10vw',
 		width: '180px',
@@ -197,77 +198,7 @@ const MoviePage = () => {
 										<strong>{movieData?.Title}</strong>
 									</Card.Title>
 									<Container className="mt-2 mb-3" fluid>
-										<Row className="mb-3">
-											<Col>
-												<Card.Title className="mb-4 fs-5 d-flex align-items-center p-0">
-													{movieData?.Year}
-													<span className="mx-3 border b-1 p-1 rounded border-dark fs-6">
-														{movieRate(movieData?.Rated)}
-													</span>
-													<span>{movieData?.Runtime}</span>
-												</Card.Title>
-											</Col>
-										</Row>
-										<Container className="ms-0 mb-3" fluid>
-											<Button
-												variant="warning"
-												onClick={() => setOpenDescription(!openDescription)}
-												aria-controls="description-section"
-												aria-expanded={openDescription}
-											>
-												Read more
-											</Button>
-										</Container>
-										<Collapse in={openDescription}>
-											<Row id="description-section">
-												<Col>
-													<Row className="mb-3">
-														<Card.Title className="fs-3">Plot</Card.Title>
-														<Card.Text style={{ color: '#333' }}>
-															{movieData?.Plot}
-														</Card.Text>
-													</Row>
-													<Row>
-														<div className="d-flex align-items-center mb-1">
-															<Card.Title className="m-0 p-0">Imdb:</Card.Title>
-															<Card.Text className="fs-5 ms-1">
-																{movieData?.imdbRating}
-															</Card.Text>
-														</div>
-														<div className="d-flex align-items-center ">
-															<Card.Title className="m-0 p-0">
-																Country:
-															</Card.Title>
-															<Card.Text className="fs-5 ms-1">
-																{movieData?.Country}
-															</Card.Text>
-														</div>
-													</Row>
-												</Col>
-												<Col>
-													<Row className="mb-3">
-														<Card.Title>
-															<span>Actors:</span>{' '}
-															<strong>{movieData?.Actors}</strong>
-														</Card.Title>
-														<Card.Title>
-															<span>Director:</span>{' '}
-															<strong>{movieData?.Director}</strong>
-														</Card.Title>{' '}
-													</Row>
-													<Row>
-														<Card.Title>
-															<span>Category:</span>{' '}
-															<strong>{movieData?.Genre}</strong>
-														</Card.Title>{' '}
-														<Card.Title>
-															<span>Language:</span>{' '}
-															<strong>{movieData?.Language}</strong>
-														</Card.Title>
-													</Row>
-												</Col>
-											</Row>
-										</Collapse>
+										<MovieInfo movieData={movieData} />
 										<hr />
 										<Row>
 											<Col>
