cf26cc02fa431216ba879d8d6eaec69b893a848a
Ville Niemi <vilniemi@gmail.com>
2022-11-02 16:44:00 +0200

    user name in navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,11 +3,13 @@ import { Container, Navbar, Image, Nav, Button } from 'react-bootstrap';
 import { MdLogout } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 import { RootReducer } from '../../types/appTypes';
+import { useSession } from 'next-auth/react';
 
 const NavigationBar = () => {
 	const LogoPng = '/logo-hypertube/logo-no-background.png';
 	const userInStore = useSelector((state: RootReducer) => state.userReducer);
-	
+	const { data: session } = useSession();
+
 	return (
 		<>
 			<Navbar
@@ -34,11 +36,18 @@ const NavigationBar = () => {
 							</Nav.Link>
 							<Nav.Item className="ms-md-auto me-3 d-none d-md-block">
 								<Navbar.Text className="fs-5">
-									{userInStore?.userName}
+									{/* {userInStore?.userName} */}
+									{session && session.user?.name}
 								</Navbar.Text>
 							</Nav.Item>
 							<Nav.Item className="d-none d-md-block">
-								<Button onClick={() => signOut({ callbackUrl: 'http://localhost:3000' })} size="sm" variant="warning">
+								<Button
+									onClick={() =>
+										signOut({ callbackUrl: 'http://localhost:3000' })
+									}
+									size="sm"
+									variant="warning"
+								>
 									<MdLogout className="fs-5" />
 								</Button>
 							</Nav.Item>
