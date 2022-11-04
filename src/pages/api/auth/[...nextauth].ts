fc7db4a57dd329da2fc0f9de18843a61e75c44fb
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-11-04 12:53:24 +0200

    Finalized image endpoint and added comments to nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,6 +21,7 @@ export const authOptions: NextAuthOptions = {
 			// You can specify whatever fields you are expecting to be submitted.
 			// e.g. domain, username, password, 2FA token, etc.
 			// You can pass any HTML attribute to the <input> tag through the object.
+			// This property needs to exists, but the object doesn't have to defined if we're using our custom login page.
 			credentials: {},
 			async authorize(credentials: any) {
 				// You need to provide your own logic here that takes the credentials
@@ -35,6 +36,7 @@ export const authOptions: NextAuthOptions = {
 					where: { email: credentials.email },
 				});
 				console.log('user from db', user);
+				// User is unauthorized and login is prevented if no user is found or there's no password or user id. If user exists, but there's no password, it means user has signed up with OAuth provider instead of credentials. We can't return anything other than null, though, so it's just generic error in client.
 				if (!user || !user.password || user.id.length < 1) return null;
 				// Check password validity
 				const validPassword = await verify(user.password, credentials.password);
@@ -43,6 +45,7 @@ export const authOptions: NextAuthOptions = {
 					id: user.id,
 					email: user.email,
 					name: user.name,
+					emailVerified: user.emailVerified,
 				});
 				return {
 					id: user.id,
@@ -73,14 +76,21 @@ export const authOptions: NextAuthOptions = {
 		}),
 		// ...add more providers here
 	],
+	// Define pages where we want to use our own custom pages. Only necessary we're not ok with the built in version.
 	pages: {
 		signIn: '/login',
+		// signOut: '/auth/signout',
+		// error: '/auth/error', // Error code passed in query string as ?error=
+		// verifyRequest: '/auth/verify-request', // (used for check email message)
+		// newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
 	},
 	session: {
 		// Must be set to jwt for sessions to work properly with credentials provider
 		strategy: 'jwt',
 	},
+	// Custom callbacks to override standard behavior
 	callbacks: {
+		// Handles what information gets added to the token. This can be removed if we don't need to add anything. It's needed because we can't add extended user data to session otherwise.
 		async jwt({ token, user, account }) {
 			// Persist the OAuth access_token and or the user id to the token right after signin
 			if (user) {
@@ -92,19 +102,22 @@ export const authOptions: NextAuthOptions = {
 			}
 			return token;
 		},
+		// This callback allow us to choose what information is stored in session. We're adding token, because that's where our extended user information is stored. This can be removed if we don't need to add anything.
 		async session({ session, token }) {
-			// Send properties to the client, like an access_token and user id from a provider.
 			session.token = token;
 			return session;
 		},
+		// This callback defines login logic. We can approve or deny login depending on user and account data, and redirect to user to appropriate page when denied login. This is not necessary, if we're ok with standard logic.
 		async signIn({ user, account, profile }) {
 			console.log('user in signin callback', user);
 			// Check if user's email has been verified
 			console.log('account in signin callback', account);
+			// This is necessary trick to avoid type errors when accessing user properties
 			const adapterUser = user as AdapterUser;
 			// OAuth providers are trusted by default. Other OAuth providers should be added here too.
 			if (account?.provider === '42-school' || account?.provider === 'github')
 				return true;
+			// Email login is allowed only for users who have signed up and verified their email.
 			if (account?.provider === 'email') {
 				if (adapterUser.name && adapterUser.emailVerified)
 					return true;
@@ -121,6 +134,7 @@ export const authOptions: NextAuthOptions = {
 			// return '/unauthorized'
 		},
 	},
+	// Turn on for debugging
 	debug: true,
 };
 
