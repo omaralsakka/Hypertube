ea6e58f999f77b9d8a882149f7438dbf0fa34c34
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-11-04 15:16:00 +0200

    Image upload from setting page works now. Old picture gets deleted when new is uploaded.

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -7,26 +7,30 @@ import { prisma } from '../../server/db/client';
 const MAX_FILE_SIZE = 500000;
 const ACCEPTED_FILE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
 
-const uploadFormSchema = z.object({
-	name: z.string().min(1),
-	email: z.string().email(),
-	image: z
-		.any()
-		.refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
-		.refine(
-			(file) => ACCEPTED_FILE_TYPES.includes(file?.type),
-			'Only .jpg, .jpeg, and .png formats are supported.'
-		),
-});
+const EmailSchema = z.string().email();
+const FileSchema = z
+	.any()
+	.refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+	.refine(
+		(file) => ACCEPTED_FILE_TYPES.includes(file?.type),
+		'Only .jpg, .jpeg, and .png formats are supported.'
+	);
+
+const validateInput = async(email: string | undefined, file: File) => {
+	try {
+		const validEmail = EmailSchema.parse(email);
+		const validFile = FileSchema.parse(file)
+		if (!validEmail || !validFile) return false
+		return true
+	} catch (err) {}
+}
+
 export default function image(req: NextApiRequest, res: NextApiResponse) {
 	try {
 		// Validate token
 
 		if (req.method === 'POST') {
 			// Upload file
-			const { body } = req;
-			const isValid = uploadFormSchema.parse(body);
-			if (!isValid) return res.status(400).send('Invalid input data');
 			const form = new formidable.IncomingForm();
 			// Parse form and save files
 			form.parse(req, async function (err, fields, files) {
@@ -34,14 +38,17 @@ export default function image(req: NextApiRequest, res: NextApiResponse) {
 				if (typeof files === 'undefined')
 					return res.status(400).send('Missing image');
 				if (!fields.email) return res.status(400).send('Missing email address');
-				let file: File;
-				if (Array.isArray(files)) file = files[0];
-				else file = files as unknown as File;
+				let file: File | undefined;
+				if (Array.isArray(files.files)) file = files.files[0];
+				else file = files.files as unknown as File;
 				if (typeof file === 'undefined') return;
 				let email;
 				if (Array.isArray(fields.email)) email = fields.email[0];
 				else email = fields.email;
-				await saveFile(file);
+				if (!validateInput(email, file)) return res.status(400).send('Invalid input variables')
+				// Create new filename
+				const filename = `${file.newFilename}.${file.mimetype?.split("/").pop()}`
+				await saveFile(file, filename);
 				// Get current image
 				const oldImage = await prisma.user.findUnique({
 					where: {
@@ -54,23 +61,29 @@ export default function image(req: NextApiRequest, res: NextApiResponse) {
 				// Write new filename to DB
 				const photo = await prisma.user.update({
 					data: {
-						image: file.newFilename,
+						image: filename,
 					},
 					where: {
 						email: email,
 					},
 				});
-				if (!photo) return res.status(500).send('Failed to write filename to database')
+				if (!photo)
+					return res.status(500).send('Failed to write filename to database');
+				if (oldImage?.image === '')
+					return res.status(201).send('Image updated successfully');
 				// If old image is local, delete it
 				if (oldImage?.image?.search('http') !== 0) {
+					console.log('Deleting old image', oldImage)
 					unlink(`./public/images/${oldImage?.image}`, (err) => {
 						if (err) {
 							console.error(err);
-							return res.status(500).json({ message: 'Could not delete old image' });
+							return res
+								.status(500)
+								.json({ message: 'Could not delete old image' });
 						}
 					});
 				}
-				
+
 				return res.status(201).send('Image updated successfully');
 			});
 		}
@@ -83,9 +96,12 @@ export default function image(req: NextApiRequest, res: NextApiResponse) {
 	}
 }
 
-const saveFile = async (file: File) => {
-	const data = fs.readFileSync(file.filepath);
-	fs.writeFileSync(`./public/images/${file.newFilename}`, data);
+const saveFile = async (file: File, filename: string) => {
+	console.log('Trying to save the file', file);
+	const data = fs.readFileSync(file.filepath)
+	console.log('File read successfully');
+	fs.writeFileSync(`./public/images/${filename}`, data);
+	console.log('File written successfully');
 	fs.unlinkSync(file.filepath);
 	return;
 };
