1fcd7a7d0664cd019c1b2f895fc3054e0d159f9f
“Omar <djomrofficial@gmail.com>
2022-11-08 11:56:46 +0200

    removed movieInfo folder as movieDescription was the same component

---
+++ src/components/loadingLogo/index.tsx
@@ -0,0 +1,18 @@
+import { Container, Image } from 'react-bootstrap';
+
+const LoadingLogo = () => {
+	return;
+	<>
+		<Container>
+			<Container>
+				<Image
+					className=""
+					src="./logo-hypertube/logo-no-background.png"
+					alt="hypertube logo"
+				/>
+			</Container>
+		</Container>
+	</>;
+};
+
+export default LoadingLogo;
