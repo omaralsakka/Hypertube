bb8afc82d77c0f3f6e719e130236bc0bb775830a
Ville Niemi <vilniemi@gmail.com>
2022-11-24 16:01:36 +0200

    search replace for quotes

--- utils/search_replace_quote.py
+++ utils/search_replace_quote.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import csv
 
-
-
+df = pd.read_csv('test_movies.csv')
 # Select column (can be A,B,C,D)
-col = 'genres';
+
 
 # Find and replace values in the selected column
-df[col] = df[col].replace(''', '"')
+#df['genres'] = df['genres'].str.replace('\"','') )
+df['genres'] = df['genres'].str.replace(r"'", '\"')
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+#   print(df['genres'])
+df.to_csv('test_movies_corrected.csv', encoding='utf-8', index=False,  quoting=csv.QUOTE_NONE, quotechar="",  escapechar="\\")
