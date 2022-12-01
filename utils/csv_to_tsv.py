1f07071b78753827cea8f0162b0d21f2e43402f8
Ville Niemi <vilniemi@gmail.com>
2022-12-01 07:32:52 +0200

    cleanup

--- utils/csv_to_tsv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('test_movies.csv','r') as csvin, open('test_movies.tsv', 'w') as tsvout:
-    csvin = csv.reader(csvin)
-    tsvout = csv.writer(tsvout, delimiter='\t')
-
-    for row in csvin:
-        tsvout.writerow(row)
--- utils/csvconvert.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-with open('movie_genres.json', encoding='utf-8') as inputfile:
-    df = pd.read_json(inputfile)
-
-df.to_csv('movie_genres.csv', encoding='utf-8', index=False)
